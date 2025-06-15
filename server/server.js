65fa3f45512593a78012564d821279ecfc849321
Shivam <139750193+Shivvy-The-Coder@users.noreply.github.com>
2025-06-15 20:48:14 +0530

    file added

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const allowedOrigins =['https://skilltrackerapp-1.onrender.com']
 
 app.use(express.json()) //all the request will be passd through json 
 app.use(cookieParser());
-// app.use(cors({ origin:allowedOrigins,credentials:true})) //credentials true so that we can send cookies in the response from the express app
+app.use(cors({ origin:allowedOrigins,credentials:true})) //credentials true so that we can send cookies in the response from the express app
 
 
 // API EndPoints
@@ -24,7 +24,7 @@ app.get("/",(req,res)=>{res.send("API Working");})
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/skill", skillRouter)
-app.listen(port , ()=>
+app.listen(port , (req,res)=>
 {
     console.log(`Application is running on Port ${port}`);
 })
