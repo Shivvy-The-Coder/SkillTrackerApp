4e6096c60773071c8307955421cdf21e0c8bb313
Shivam <139750193+Shivvy-The-Coder@users.noreply.github.com>
2025-06-15 17:15:25 +0530

    added

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,7 +1,7 @@
 import arrow_icon from './arrow_icon.svg'
 import lock_icon from './lock_icon.svg'
 import hand_wave from "./hand_wave.png"
-import logo from "./logo.png"
+import logo from ".Logo.png"
 import person_icon from "./person_icon.svg"
 import mail_icon from "./mail_icon.svg"
 // import logo1 from "./logo.svg"
